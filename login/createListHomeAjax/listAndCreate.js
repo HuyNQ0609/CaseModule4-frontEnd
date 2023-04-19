63fe67239714c57878074329741030fd51faa055
Tuong <aumanhtuong09122001@gmail.com>
2023-04-19 09:02:08 +0700

    dang hoan thien phan create home

--- login/createListHomeAjax/listAndCreate.js
+++ login/createListHomeAjax/listAndCreate.js
@@ -19,8 +19,8 @@ function showAllHome() {
                                     <span class="proerty-price pull-right"><input type="text" value="${data[i].price}"></span>
                                     <p><input type="text" value="${data[i].description}"></p>
                                     <div class="property-icon">
-                                        <img src="assets/img/icon/bed.png">${data[i].numberOfBedroom}
-                                        <img src="assets/img/icon/shawer.png">${data[i].numberOfBathroom}
+                                        <img src="assets/img/icon/bed.png" alt="">${data[i].numberOfBedroom}
+                                        <img src="assets/img/icon/shawer.png" alt="">${data[i].numberOfBathroom}
                                     </div>
                                 </div>
                                 </div>
@@ -34,21 +34,59 @@ function showAllHome() {
 
 function addNewHome() {
     let name = $('#name').val()
-    let types = $('#types').val()
+    let types = arrayHomeType()
     let address = $('#address').val()
-    let numberOfBathroom = $('#numberOfBathroom').val()
-    let numberOfBedroom = $('#numberOfBedroom').val()
-    let description = arrayPicture().values()
-    const arrayImg = document.getElementsByName('image');
-    console.log(arrayImg);
+    let numberOfBathroom = $('#bath').val()
+    let numberOfBedroom = $('#bedr').val()
+    let description = $('#description').val()
+    let price = $('#price').val()
+    let arrayImg = arrayPicture();
+    let newHome = {
+        name: name,
+        type: arrayHomeType(),
+        address: address,
+        numberOfBedroom: numberOfBedroom,
+        numberOfBathroom: numberOfBathroom,
+        description: description,
+        price: price,
+        arrayImg: arrayPicture()
+    };
+    console.log(newHome)
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        data: JSON.stringify(newHome),
+        url: "http://localhost:8080/homes",
+        success() {
+            showAllHome()
+        }
+    });
+    event.preventDefault();
 
+}
+
+function arrayHomeType() {
+        var input = document.getElementsByClassName("check")
+        var value = [];
+        for (let i = 0; i < input.length; i++) {
+            value[i] = {
+                "name": input[i].value
+            }
+        }
+    return value;
 }
 
 function arrayPicture() {
     var input = document.getElementsByName('image');
-    var value = new Array();
+    var value = [];
     for (let i = 0; i < input.length; i++) {
-        value[i] = input[i].value;
+        value[i] = {
+            "src": input[i].value
+        }
+
     }
     return value;
 }
@@ -56,10 +94,11 @@ function arrayPicture() {
 
 function inputPicture() {
     let context = document.getElementById("inputPicture").innerHTML
-    context += `<div class="form-group">
-                                            <label for="property-images">Chose Images :</label>
-                                            <input class="form-control" type="file" id="property-images" multiple>
-                                        </div>`
+    context += ` <div id="inputPicture">
+<div class="form-group">
+                <label>Chose Images :</label>
+                <input type="file"  name="image" id="image">
+                </div></div>`
     document.getElementById("inputPicture").innerHTML = context
     event.preventDefault()
 }
